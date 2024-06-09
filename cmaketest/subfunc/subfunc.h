7fdd14330947e87bde43f0ab075dac4813821ebe
licoded <busy.li@foxmail.com>
2024-06-09 19:50:10 +0800

    cmaketest: add subfunc/subfunc.(h|cpp) + subfunc/subsubfunc/subsubfunc.(h|cpp)

---
+++ cmaketest/subfunc/subfunc.h
@@ -0,0 +1,7 @@
+#ifndef __SUB_FUNC_H__
+#define __SUB_FUNC_H__
+
+int subfunc1(void);
+int subfunc2(void);
+
+#endif
---
+++ cmaketest/subfunc/subsubfunc/subsubfunc.h
@@ -0,0 +1,7 @@
+#ifndef __SUB_SUB_FUNC_H__
+#define __SUB_SUB_FUNC_H__
+
+int subsubfunc1(void);
+int subsubfunc2(void);
+
+#endif
